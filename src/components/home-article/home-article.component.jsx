31c4f349b2567126f3297a817e2167221d5e5691
Yue Wang <yuewang@lulus-mbp.myfiosgateway.com>
2020-09-22 09:35:00 -0400

    add fact and fiction on home page

--- src/components/home-article/home-article.component.jsx
+++ src/components/home-article/home-article.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Stop from '../../assets/stop.jpeg';
-import Newsday from '../../assets/newsday.mp4';
+import Fact1 from '../../assets/fact1.png';
+import Fact2 from '../../assets/fact2.png';
 import Mission from '../mission/mission.component';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 import './home-article.styles.scss';
 const HomeArticle = () => {
+  const matches = useMediaQuery('(max-width:650px)');
   return (
     <div className="home-article">
       <Mission />
@@ -13,11 +16,15 @@ const HomeArticle = () => {
         title="rally"
         src="https://player.vimeo.com/video/460429175"
         width="100%"
-        height="564"
+        // height="564"
+        height={matches ? '400' : '564'}
         frameborder="0"
         allow="autoplay; fullscreen"
         allowfullscreen
       ></iframe>
+      <br />
+      <img src={Fact1} alt="fact1" className="image-shelter" />
+      <img src={Fact2} alt="fact2" className="image-shelter" />
     </div>
   );
 };
