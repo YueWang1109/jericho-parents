013f6d32c820360a2981d4aa75d974ca15d560e1
Yue Wang <yuewang@lulus-mbp.myfiosgateway.com>
2020-10-07 13:26:56 -0400

    update facts

--- src/components/fiction-fact/fiction-fact.component.jsx
+++ src/components/fiction-fact/fiction-fact.component.jsx
@@ -81,8 +81,92 @@ const factData = [
   },
 ];
 
+const lauraData = [
+  {
+    rumor:
+      'This is a Homeless Shelter that will house criminals, sex offenders and drug addicts',
+    lauraFact:
+      'The Jericho Family Support Center is strictly for families with children',
+    fact:
+      'As Stated by Nancy Nunziatta on September 10, 2020 at the public hearing, criminals will NOT be turned away.  In addition, no one will commit that this will stay a family facility for more than a year - it will house whatever homeless individuals are the most in need each year.CHI signed a ten-year lease with the owner. The facility can be changed from a family center to a shelter for singles.',
+  },
+  {
+    rumor:
+      'Crime will go up and criminals will be roaming the streets of our neighborhood and bring drugs into our schools',
+    lauraFact:
+      'Creating this type of facility to serve families will ultimately eliminate the other “welfare” hotels in the Jericho area that do not provide services and are unregulated and unsupervised',
+    fact:
+      'There was no formal statement by Nassau County that the “welfare hotels” will be shut down.  We have asked this direct question several times, and have received no response.  We need it in writing if the intent is to close the Jericho Motels.',
+  },
+  {
+    rumor: 'These families will be living there indefinitely for years',
+    lauraFact:
+      'CHI has an innovative system to deal with homelessness which sets a “blue ribbon” standard for homeless families designed to get them back on their feet and into permanent homes in a documented average of six to nine months.',
+    fact:
+      'The amount of money being spent on this location lease is double the market rate, directly taking funds away from the homeless.  The developer, Mr. Shafran, is recouping his money on this investment in 4 years.  That is three times as fast as most other commercial real estate deals. This question  was asked during the legislature’s hearing.  Nancy Nunziatta did not provide any plan to help the homeless family to get permanent homes in 6-9 months.',
+  },
+  {
+    rumor: 'Hundreds of children will enter our schools and stay indefinitely',
+    lauraFact:
+      'Of the 80 families that have been interviewed for placement in this facility, parents of only 19 children have expressed interest in registering their children in the Jericho schools.',
+    fact:
+      'This is ONE cohort of families, that according to the above answer will completely change over every year.  We have NO WAY of knowing what future families moving into the facility will elect to do. If a family is then moved to temporary housing again, and not permanent, they are allowed to stay in the Jericho school district until they find permanent housing.',
+  },
+  {
+    rumor: 'Jericho’s #1 ranking will drop because of new homeless students',
+    lauraFact:
+      'Jericho’s #1 ranking will drop because of new homeless students. There have been children who are homeless attending Jericho schools for many years, up to 45 students at one point, and the ranking has never changed because their commitment has remained steadfast and unwavering.  Neither race, religion, creed, sexual orientation, heritage, economic status nor the ability of the parents to speak English has been a factor in the district’s commitment to our children.',
+    fact:
+      '1) No one in our group has raised this concern. 2)Mr. Bill Ferro compared the homeless coming to our district for 6-9 months at a time, to some families who do not speak English well in our community.  This is discriminatory and racist, and is being repeated by Laura Curran!',
+  },
+  {
+    rumor:
+      'The school district knew about this the whole time and kept the community in the dark',
+    lauraFact:
+      'There were preliminary discussions with the school district several months ago; however, there was nothing for the school district to communicate with the community. The Hampton Inn was only acquired in July, and was in review by the New York State Office of Temporary and Disability Assistance.  The state inspects and certifies such facilities and typically does not consult the local community',
+    fact:
+      'Our superintendent stated in an email that went to the entire district that he had an initial meeting in December 2019.  There was another meeting in January where he was told to keep this quiet.  This impacts the community and the community has a right to know and give input in major decisions such as this one. On a different point, if the Hampton Inn was acquired in July, how was the MOU amended in June, [and the lease signed in June]?',
+  },
+  {
+    rumor:
+      'It is unsuitable because there is no grocery store within walking distance and no crosswalks',
+    lauraFact:
+      'Three meals a day will be provided to these families. They do not have stoves in their rooms for cooking, so trips to the grocery stores would be unnecessary.  Crosswalks are close by on Jericho Turnpike at safe crossing areas like by the Dunkin Donuts and The Home Depot.  In addition, there will be a van and bus service providing trips to jobs and appointments for the families',
+    fact:
+      'Was an impact studied completed to prove the response, or is this the opinion of Laura Curran? Jericho Turnpike is an extremely dangerous road, having at least 3 lanes on each side.  It is not an appropriate setting for children to be crossing.  The road has a 4-way intersection right in front of the building',
+  },
+  {
+    rumor: 'This will destroy our community and tear us apart',
+    lauraFact:
+      'Only actions of hate based on fear can do that.  Examples of this would be 1) illegally protesting at our community grocery store (Whole Foods is on private property) with a petition to stop the “homeless shelter” under the guise of looking out for the rights of the homeless 2) putting up signs of hate throughout our peaceful community under the guise of need for “transparency” with the hateful message of “say NO to homeless shelter” or 3) sending hate filled, sometimes racist emails to our Board of Education, Superintendent and our elected officials.  These actions run in direct opposition to the welcoming, caring community we know Jericho to be, and is a misrepresentation of the values at the core of our community.  We are kind, inclusive and treat our neighbors with dignity and respect.  So, in facing our fears we put this website together to keep our community informed with TRUTHS based in reality, rather than rumors based on fear. Our community can UNITE under the umbrella of KINDNESS.  Visit https://supportjerichofamilies.org/ or https://communityhousing.org/ for more information.',
+    fact:
+      'Why is Laura Curran advertising for a private organization established by a 20 year old Jericho Graduate? The statements made are False.  The Jericho community is the victim.  We have been called racists and haters by the BOE.  We have been kept in the dark with regards to plans for erecting a shelter in our community without any notice, community vote, information, studies etc. Protests are not illegal.  We have a right to protest.  This is our First Amendment Right.  All protests have been done peacefully. Signs that have been put up are specifically in opposition to an illegal homeless shelter.  This community is not opposed to the homeless. This plan of conversion from a hotel to transitional housing does not comply with longstanding Town of Oyster Bay laws which were designed to protect the health and safety of our community.  The preliminary injunction was granted. Moreover, the lack of transparency and the explicit actions taken to conceal this mega shelter project from the public were an utter disgrace.',
+  },
+];
+
 const FictionFact = () => (
   <div className="fiction-fact">
+    <h1>
+      Laura Curren's Truth <i style={{ fontSize: '24px' }}>vs.</i> The Real
+      Truth
+    </h1>
+    <h3>
+      Updated Facts re: Laura Curran’s newest Rumor vs Facts （updated October
+      6, 2020)
+    </h3>
+    {lauraData.map((item) => (
+      <div className="fiction-fact-container">
+        <div className="rumor">
+          <b>Fear/Rumor:</b> {item.rumor}
+        </div>
+        <div className="laura-truth">
+          <b>Laura Curran's Truth:</b> {item.lauraFact}
+        </div>
+        <div className="truth">
+          <b>The Real Truth:</b> {item.fact}
+        </div>
+      </div>
+    ))}
     <h1>Fiction vs Fact</h1>
     {factData.map((item) => (
       <div className="fiction-fact-container">
